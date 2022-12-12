038c786b76fca26d919ba48dc2cdba996a508dec
Ameer Deen <writeameer@gmail.com>
2022-12-12 20:58:56 +0400

    Added empty pulumi program

---
+++ Azure/AzureInitializer.cs
@@ -0,0 +1,22 @@
+﻿namespace ArkInit.Azure;
+
+public static class AzureInitializer
+{
+    public static Dictionary<string, object?> DoStuff()
+    {
+        var exports = new Dictionary<string, object?>();
+
+
+        return exports;
+    }
+
+    public static bool CheckConfig()
+    {
+        var configOK = false;
+
+        var config = new Pulumi.Config();
+        var location = config.Require("location");
+        Console.WriteLine($"The location was:{location}");
+        return configOK;
+    }
+}
